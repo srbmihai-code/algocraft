e8da9e4179771fc78d8718331d885ea136697539
Mihai Surubaru <srbmihai.code@gmail.com>
2026-01-16 21:18:40 +0200

    Improved level page visual interface

---
+++ frontend/src/utils/getNamesFromURL.ts
@@ -0,0 +1,25 @@
+import levelsData from "../utils/levels.json";
+
+interface Names {
+  chapterName?: string;
+  levelName?: string;
+}
+
+export default function getNamesFromURLs(
+  chapterURL?: string,
+  levelURL?: string
+): Names {
+  if (!chapterURL) return {};
+
+  const chapter = levelsData.find((c) => c.chapterURL === chapterURL);
+  if (!chapter) return {};
+
+  const level = levelURL
+    ? chapter.levels.find((l) => l.levelURL === levelURL)
+    : undefined;
+
+  return {
+    chapterName: chapter.chapterName,
+    levelName: level?.levelName,
+  };
+}
